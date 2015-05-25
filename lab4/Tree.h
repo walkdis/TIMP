f3334180c380f28c4942944f2fc07dd6ac66ba02
walkdis <walkdis.as@gmail.com>
2015-05-25 16:35:41 +0300

    Update Tree.h

--- lab4/Tree.h
+++ lab4/Tree.h
@@ -12,21 +12,18 @@ using namespace std;
 #include "Tree.h"
 
 class Tree {
-public:
-	Tree();
-	Tree(int value, Tree* parent);
-
-	void addChild(Tree * elem);
-	void printChild() const;
-  void printOne() const;
-  void printTwo() const;
-  void printThree() const;
-  int getChildCount() const;
-
 private:
-
 	int value;
 	Tree* parent;
 	Tree** child;
 	int  childCount;
+public:
+  Tree();
+  Tree(int value, Tree* parent);
+  void addChild(Tree * elem);
+  void printChild() const;
+  void printOne() const;
+  void printTwo() const;
+  void printThree() const;
+  int getChildCount() const;
 };
