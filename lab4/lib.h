a184c8353f7a50a59a5a935f3976da41c5e20611
walkdis <walkdis.as@gmail.com>
2015-05-25 16:18:23 +0300

    Create lib.h

---
+++ lab4/lib.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <tchar.h>
+#include <time.h>
+#include <stdio.h>
+#include <locale.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+#include <memory>
+#include <cassert>
+
+#pragma warning(disable: 4996)
+#pragma warning(disable: 4018)
+#pragma comment(lib, "User32")
+#pragma comment(lib, "Gdi32")
+#pragma comment(lib, "Shell32")
+#pragma comment(lib, "Comdlg32")
+
+
+#include <Windows.h>
+#include <Windowsx.h>
+
+#include <vector>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+#include "Tree.h"
